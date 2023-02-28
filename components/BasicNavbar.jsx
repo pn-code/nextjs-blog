44b6d9ed398268b67fbf4d71cbb88fee7134219d
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-27 21:42:11 -0800

    Edited Links

--- components/BasicNavbar.jsx
+++ components/BasicNavbar.jsx
@@ -10,8 +10,8 @@ function BasicNavbar() {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Login</Nav.Link>
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/login">Login</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
