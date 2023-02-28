34925012d4c7b676643cf07ab07065bb5cdb7290
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-27 21:43:01 -0800

    Moved Navbar to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import BasicNavbar from "@/components/BasicNavbar";
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <BasicNavbar />
+            <Component {...pageProps} />
+        </>
+    );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
 import Header from "@/components/Header";
-import BasicNavbar from "@/components/BasicNavbar";
 
 export default function Home() {
     return (
         <>
             <Header />
-            <BasicNavbar/>
         </>
     );
 }
