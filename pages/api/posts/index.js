9d12072d4193377d04764fe62003175a9542f314
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-28 15:53:01 -0800

    Built out POST endpost for posts

--- pages/api/posts/index.js
+++ pages/api/posts/index.js
@@ -10,5 +10,18 @@ export default async function handler(req, res) {
         } catch (error) {
             res.status(500).json(error);
         }
+    } else if (method === "POST") {
+        try {
+            const { title, content } = req.body;
+            const data = await prisma.post.create({
+                data: {
+                    title,
+                    content,
+                },
+            });
+            res.status(200).json(data);
+        } catch (error) {
+            res.status(500).json(error);
+        }
     }
 }
