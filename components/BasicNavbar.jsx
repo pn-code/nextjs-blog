6e5c5c1f29fab9453ddb0519aea920b37c6afde4
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-27 22:07:05 -0800

    Added new post link to navbar

--- components/BasicNavbar.jsx
+++ components/BasicNavbar.jsx
@@ -12,6 +12,7 @@ function BasicNavbar() {
                     <Nav className="me-auto">
                         <Nav.Link href="/">Home</Nav.Link>
                         <Nav.Link href="/login">Login</Nav.Link>
+                        <Nav.Link href="/new-post">New Post</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
