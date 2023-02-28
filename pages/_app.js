f640301b2fa52210324303a59aa25fd5ed4f3272
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-27 21:24:40 -0800

    Clearing out app and index

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,5 @@
-import "@/styles/globals.css";
-import "bootstrap/dist/css/bootstrap.css";
-import { useEffect } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App({ Component, pageProps }) {
-    useEffect(() => {
-        import("bootstrap/dist/js/bootstrap");
-    }, []);
     return <Component {...pageProps} />;
 }
