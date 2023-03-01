4a33d06c35c306e3f1d1e933c4c680381a030f2d
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-01 13:04:36 -0800

    Added comments relation to post fetch

--- pages/api/posts/[id].js
+++ pages/api/posts/[id].js
@@ -1,19 +1,20 @@
 import prisma from "@/prisma/client";
 
 export default async function handler(req, res) {
-  const { method } = req;
+    const { method } = req;
 
-  if (method === "GET") {
-    try {
-      const postId = req.query.id;
-      const data = await prisma.post.findUnique({
-        where: {
-          id: postId,
-        },
-      });
-      return res.status(200).json(data);
-    } catch (error) {
-      res.status(500).json(error);
+    if (method === "GET") {
+        try {
+            const postId = req.query.id;
+            const data = await prisma.post.findUnique({
+                where: {
+                    id: postId,
+                },
+                include: { comments: true },
+            });
+            return res.status(200).json(data);
+        } catch (error) {
+            res.status(500).json(error);
+        }
     }
-  }
 }
