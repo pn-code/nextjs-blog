34c044a1c540e7c5ac2f7c40bfae992ef2083230
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-01 11:28:35 -0800

    Checking cookies for admin & passing to Feed

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,26 @@
 import Header from "@/components/Header";
 import Feed from "@/components/Feed";
 
-export default function Home() {
+export default function Home({ admin }) {
     return (
         <>
             <Header />
-            <Feed/>
+            <Feed admin={admin} />
         </>
     );
 }
+
+export const getServerSideProps = async (ctx) => {
+    const myCookie = ctx.req?.cookies || "";
+    let admin = false;
+
+    if (myCookie.token === process.env.TOKEN) {
+        admin = true;
+    }
+
+    return {
+        props: {
+            admin,
+        },
+    };
+};
