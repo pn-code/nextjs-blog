b7606d70eaf9358752eb9f8884a5936b038a9a91
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-01 13:05:07 -0800

    Rendering comments info

--- components/CommentCard.jsx
+++ components/CommentCard.jsx
@@ -1,16 +1,14 @@
-import Card from 'react-bootstrap/Card';
+import Card from "react-bootstrap/Card";
 
-const CommentCard = () => {
-  return (
-    <Card>
-      <Card.Body>
-        <Card.Title>Username</Card.Title>
-        <Card.Text>
-          Here's some feedback left by a visitor of the website.
-        </Card.Text>
-      </Card.Body>
-    </Card>
-  )
-}
+const CommentCard = ({ comment }) => {
+    return (
+        <Card>
+            <Card.Body>
+                <Card.Title>{comment.author}</Card.Title>
+                <Card.Text>{comment.content}</Card.Text>
+            </Card.Body>
+        </Card>
+    );
+};
 
-export default CommentCard
+export default CommentCard;
