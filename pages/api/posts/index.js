ebbc49c8044d971ec0f66b78723db59a67c91221
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-28 20:23:54 -0800

    Adding authorId to POST endpoint post creation

--- pages/api/posts/index.js
+++ pages/api/posts/index.js
@@ -1,27 +1,28 @@
 import prisma from "@/prisma/client";
 
 export default async function handler(req, res) {
-    const { method } = req;
+  const { method } = req;
 
-    if (method === "GET") {
-        try {
-            const data = await prisma.post.findMany();
-            return res.status(200).json(data);
-        } catch (error) {
-            res.status(500).json(error);
-        }
-    } else if (method === "POST") {
-        try {
-            const { title, content } = req.body;
-            const data = await prisma.post.create({
-                data: {
-                    title,
-                    content,
-                },
-            });
-            res.status(200).json(data);
-        } catch (error) {
-            res.status(500).json(error);
-        }
+  if (method === "GET") {
+    try {
+      const data = await prisma.post.findMany();
+      return res.status(200).json(data);
+    } catch (error) {
+      res.status(500).json(error);
     }
+  } else if (method === "POST") {
+    try {
+      const { title, content, authorId } = req.body;
+      const data = await prisma.post.create({
+        data: {
+          title,
+          content,
+          authorId,
+        },
+      });
+      res.status(200).json(data);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
 }
