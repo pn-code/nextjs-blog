919744b561cebc638a3585630dc8a5802f123528
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-28 20:24:50 -0800

    Added server side fetch of post & rendered data

--- pages/post/[id].jsx
+++ pages/post/[id].jsx
@@ -1,93 +1,47 @@
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import { Container, Row, Col } from "react-bootstrap";
 import Link from "next/link";
 import DeletePostModal from "@/components/DeletePostModal";
 import CommentCard from "@/components/CommentCard";
+import axios from "axios";
 
-const PostDetails = () => {
-    return (
-        <Container fluid className="mx-4 my-2">
-            <Row>
-                <Col>
-                    <h2>Title</h2>
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <h5>Description</h5>
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <p>
-                        "At vero eos et accusamus et iusto odio dignissimos
-                        ducimus qui blanditiis praesentium voluptatum deleniti
-                        atque corrupti quos dolores et quas molestias excepturi
-                        sint occaecati cupiditate non provident, similique sunt
-                        in culpa qui officia deserunt mollitia animi, id est
-                        laborum et dolorum fuga. Et harum quidem rerum facilis
-                        est et expedita distinctio. Nam libero tempore, cum
-                        soluta nobis est eligendi optio cumque nihil impedit quo
-                        minus id quod maxime placeat facere possimus, omnis
-                        voluptas assumenda est, omnis dolor repellendus.
-                        Temporibus autem quibusdam et aut officiis debitis aut
-                        rerum necessitatibus saepe eveniet ut et voluptates
-                        repudiandae sint et molestiae non recusandae. Itaque
-                        earum rerum hic tenetur a sapiente delectus, ut aut
-                        reiciendis voluptatibus maiores alias consequatur aut
-                        perferendis doloribus asperiores repellat." "At vero eos
-                        et accusamus et iusto odio dignissimos ducimus qui
-                        blanditiis praesentium voluptatum deleniti atque
-                        corrupti quos dolores et quas molestias excepturi sint
-                        occaecati cupiditate non provident, similique sunt in
-                        culpa qui officia deserunt mollitia animi, id est
-                        laborum et dolorum fuga. Et harum quidem rerum facilis
-                        est et expedita distinctio. Nam libero tempore, cum
-                        soluta nobis est eligendi optio cumque nihil impedit quo
-                        minus id quod maxime placeat facere possimus, omnis
-                        voluptas assumenda est, omnis dolor repellendus.
-                        Temporibus autem quibusdam et aut officiis debitis aut
-                        rerum necessitatibus saepe eveniet ut et voluptates
-                        repudiandae sint et molestiae non recusandae. Itaque
-                        earum rerum hic tenetur a sapiente delectus, ut aut
-                        reiciendis voluptatibus maiores alias consequatur aut
-                        perferendis doloribus asperiores repellat." "At vero eos
-                        et accusamus et iusto odio dignissimos ducimus qui
-                        blanditiis praesentium voluptatum deleniti atque
-                        corrupti quos dolores et quas molestias excepturi sint
-                        occaecati cupiditate non provident, similique sunt in
-                        culpa qui officia deserunt mollitia animi, id est
-                        laborum et dolorum fuga. Et harum quidem rerum facilis
-                        est et expedita distinctio. Nam libero tempore, cum
-                        soluta nobis est eligendi optio cumque nihil impedit quo
-                        minus id quod maxime placeat facere possimus, omnis
-                        voluptas assumenda est, omnis dolor repellendus.
-                        Temporibus autem quibusdam et aut officiis debitis aut
-                        rerum necessitatibus saepe eveniet ut et voluptates
-                        repudiandae sint et molestiae non recusandae. Itaque
-                        earum rerum hic tenetur a sapiente delectus, ut aut
-                        reiciendis voluptatibus maiores alias consequatur aut
-                        perferendis doloribus asperiores repellat."
-                    </p>
-                </Col>
-            </Row>
-            <Row>
-                <Col className="gap-2">
-                    <Link href="/">Back</Link>
-                    <DeletePostModal />
-                </Col>
-            </Row>
-            <Row className="mt-4">
-                <h6>Comments</h6>
-                <CommentCard />
-                <CommentCard />
-                <CommentCard />
-                <CommentCard />
-                <CommentCard />
-            </Row>
-        </Container>
-    );
+const PostDetails = ({ post }) => {
+  return (
+    <Container fluid className="mx-4 my-2">
+      <Row>
+        <Col>
+          <h2>{post.title}</h2>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <p>{post.content}</p>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="gap-2">
+          <Link href="/">Back</Link>
+          <DeletePostModal />
+        </Col>
+      </Row>
+      <Row className="mt-4">
+        <h6>Comments</h6>
+        <CommentCard />
+        <CommentCard />
+        <CommentCard />
+        <CommentCard />
+        <CommentCard />
+      </Row>
+    </Container>
+  );
 };
+
+export async function getServerSideProps({ query }) {
+  const { id: postId } = query;
+  // Fetch data from external API
+  const res = await axios.get(`http://localhost:3000/api/posts/${postId}`);
+
+  // Pass data to the page via props
+  return { props: { post: res.data } };
+}
 
 export default PostDetails;
