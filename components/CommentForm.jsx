45f99dcd4b23ab69bcd61342300fb77152bcbcb1
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-01 12:36:48 -0800

    Built out Comment Form

---
+++ components/CommentForm.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
+const CommentForm = () => {
+    const [commentAuthor, setCommentAuthor] = useState("");
+    const [commentContent, setCommentContent] = useState("");
+
+    const submitComment = (e) => {
+      e.preventDefault();
+    }
+
+    return (
+        <Form onSubmit={(e) => submitComment(e)} className="mb-3">
+            <h4>Add a Comment</h4>
+            <Form.Group className="mb-3" controlId="name">
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                    onChange={(e) => setCommentAuthor(e.target.value)}
+                    value={commentAuthor}
+                    type="text"
+                    placeholder="Enter name"
+                />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="comment">
+                <Form.Label>Comment</Form.Label>
+                <Form.Control
+                    onChange={(e) => setCommentContent(e.target.value)}
+                    value={commentContent}
+                    as="textarea"
+                    rows={3}
+                    placeholder="Enter comment"
+                />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
+    );
+};
+
+export default CommentForm;
