3b6c6baace0709e90c6b98f5c4ba5466a27171f5
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-01 17:10:42 -0800

    Built out DELETE endpoint for post

--- pages/api/posts/[id].js
+++ pages/api/posts/[id].js
@@ -2,17 +2,29 @@ import prisma from "@/prisma/client";
 
 export default async function handler(req, res) {
     const { method } = req;
+    const postId = req.query.id;
 
     if (method === "GET") {
         try {
-            const postId = req.query.id;
             const data = await prisma.post.findUnique({
                 where: {
                     id: postId,
                 },
                 include: { comments: true },
             });
-            return res.status(200).json(data);
+            res.status(200).json(data);
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    } else if (method === "DELETE") {
+        try {
+            const data = await prisma.post.delete({
+                where: {
+                    id: postId,
+                },
+                include: { comments: true },
+            });
+            res.status(200).json(data);
         } catch (error) {
             res.status(500).json(error);
         }
