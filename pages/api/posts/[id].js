e4cb1af6e16814b0cbe2349f8c724fde7ee16a2e
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-01 17:46:55 -0800

    Adding PUT endpoint for updating posts

--- pages/api/posts/[id].js
+++ pages/api/posts/[id].js
@@ -32,5 +32,27 @@ export default async function handler(req, res) {
         } catch (error) {
             res.status(500).json(error);
         }
+    } else if (method === "PUT") {
+        try {
+            const { title, content } = req.body;
+            if (req.cookies.token === process.env.TOKEN) {
+                const data = await prisma.post.update({
+                    where: {
+                        id: postId,
+                    },
+                    data: {
+                        title,
+                        content,
+                        authorId: process.env.ADMIN_ID,
+                    },
+                    include: { comments: true },
+                });
+                res.status(200).json(data);
+            } else {
+                res.status(403).json({ success: false });
+            }
+        } catch (error) {
+            res.status(500).json(error);
+        }
     }
 }
